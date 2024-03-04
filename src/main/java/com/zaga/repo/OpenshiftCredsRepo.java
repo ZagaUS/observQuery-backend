91e9b0ff1e187d1742f9fe9840ed34da445e0fe4
surendharramaraj <surendharramaraj@gmail.com>
2024-03-04 14:38:50 +0530

    user creds fixed cache issue

--- src/main/java/com/zaga/repo/OpenshiftCredsRepo.java
+++ src/main/java/com/zaga/repo/OpenshiftCredsRepo.java
@@ -2,6 +2,7 @@ package com.zaga.repo;
 
 import com.zaga.entity.queryentity.openshift.UserCredentials;
 
+import io.quarkus.cache.CacheInvalidate;
 import io.quarkus.cache.CacheResult;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import io.quarkus.mongodb.panache.PanacheQuery;
@@ -11,7 +12,8 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class OpenshiftCredsRepo implements PanacheMongoRepository<UserCredentials> {
 
-        @CacheResult(cacheName = "user-creds")
+        // @CacheResult(cacheName = "user-creds")
+        @CacheInvalidate(cacheName = "user-creds")
         public UserCredentials getUser(String username) {
         PanacheQuery<UserCredentials> data = UserCredentials.find("username=?1", username);
         UserCredentials userData = data.firstResult();
